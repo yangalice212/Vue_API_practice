2a5a97a47e86c628a68daf45d9bcf4dd5ad82640
Alice Yang <yangalice212@gmail.com>
2021-07-08 18:57:07 +0800

    correct variables name

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,6 @@
 import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js";
+const apiUrl = "https://vue3-course-api.hexschool.io/api";
+const apiPath = "yangalice212";
 
 let productModal = null;
 let delProductModal = null;
@@ -6,8 +8,6 @@ let delProductModal = null;
 createApp({
   data() {
     return {
-      apiUrl: "https://vue3-course-api.hexschool.io/api",
-      apiPath: "yangalice212",
       products: [],
       isNew: false,
       tempProduct: {
@@ -42,7 +42,7 @@ createApp({
   methods: {
     getData(page = 1) {
       axios
-        .get(`${this.apiUrl}/${this.apiPath}/admin/products?page=${page}`)
+        .get(`${apiUrl}/${apiPath}/admin/products?page=${page}`)
         .then((res) => {
           if (res.data.success) {
             this.products = res.data.products;
@@ -55,21 +55,25 @@ createApp({
         });
     },
     updateProduct() {
-      let url = `${this.apiUrl}/${this.apiPath}}/admin/product`;
+      let url = `${apiUrl}/${apiPath}/admin/product`;
       let http = "post";
       if (!this.isNew) {
-        url = `${this.apiUrl}/${this.apiPath}}/admin/product/${this.tempProduct.id}`;
+        url = `${apiUrl}/${apiPath}/admin/product/${this.tempProduct.id}`;
         http = "put";
       }
-      axios[http](url, { data: this.tempProduct }).then((res) => {
-        if (res.data.success) {
-          alert(res.data.message);
-          productModal.hide();
-          this.getData();
-        } else {
-          console.log(res.data.message);
-        }
-      });
+      axios[http](url, { data: this.tempProduct })
+        .then((res) => {
+          if (res.data.success) {
+            alert(res.data.message);
+            productModal.hide();
+            this.getData();
+          } else {
+            alert(res.data.message);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
       // if (this.isNew) {
       //   //當產品不存在，新增至產品列表
       //   this.products.push({
@@ -106,9 +110,7 @@ createApp({
     },
     delProduct() {
       axios
-        .delete(
-          `${this.apiUrl}/${this.apiPath}}/admin/product/${this.tempProduct.id}`
-        )
+        .delete(`${apiUrl}/${apiPath}/admin/product/${this.tempProduct.id}`)
         .then((res) => {
           if (res.data.success) {
             alert(res.data.message);
@@ -117,6 +119,9 @@ createApp({
           } else {
             console.log(res.data.message);
           }
+        })
+        .catch((err) => {
+          console.log(err);
         });
       //   this.products.splice(
       //     this.products.findIndex((item) => item.id === this.tempProduct.id),
